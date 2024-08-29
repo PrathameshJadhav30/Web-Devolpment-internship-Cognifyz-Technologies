2a5532cd8673ecd7b95b7d0347b68c226659c34b
Prathamesh <prathameshaj2004@gmail.com>
2024-08-29 22:25:29 +0530

    Changes

---
+++ Level-3/Task-1/Use CSS to create a basic image gallery with thumbnail images that enlarge when clicked/script.js
@@ -0,0 +1,19 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+}
+
+function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+}
+
+function currentSlide(n) {
+    showSlides(n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    for (var i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[n - 1].style.display = "block";
+}
