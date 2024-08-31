1aead31eac04caa6ffbe096d3ff0d6e540f12f28
Prathamesh <prathameshaj2004@gmail.com>
2024-08-31 21:45:31 +0530

    Changes

---
+++ Level-3/Task-2/Create a landing page for a web developer internship/script.js
@@ -0,0 +1,7 @@
+function applyNow() {
+    window.location.href = "https://cognifyz.com/internships/"; 
+}
+
+function learnMore() {
+    window.location.href = "https://cognifyz.com/contact-us/"; 
+}
